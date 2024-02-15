dc250f43337030e22df65c646ea1e118c5e80bd3
ivanovdale <dania-spb@mail.ru>
2024-02-15 21:14:22 +0300

    setup navigation controller

--- ViperExample/Sources/App/SceneDelegate.swift
+++ ViperExample/Sources/App/SceneDelegate.swift
@@ -12,8 +12,11 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
+        let initialViewController = GeneratorViewController()
+        let navigationController = UINavigationController(rootViewController: initialViewController)
+
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = GeneratorViewController()
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
     }
 }
