07280e6d4fcbcdb1c2b42de167237a8947013c34
ivanovdale <dania-spb@mail.ru>
2024-02-15 13:41:55 +0300

    remove main storyboard

--- ViperExample/NumberViewController.swift
+++ ViperExample/NumberViewController.swift
@@ -7,13 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class NumberViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
